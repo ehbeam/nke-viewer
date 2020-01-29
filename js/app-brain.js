27614309a61cb1695f7ddfbd305d634656947a90
Elizabeth Beam <elizabeth.h.beam@gmail.com>
2020-01-29 12:57:38 -0800

    Structures weighted by PMI and thresholded by FDR

--- js/app-brain.js
+++ js/app-brain.js
@@ -5,30 +5,30 @@ var nke_palette = ["nke_blue", "nke_magenta", "nke_yellow", "nke_green", "nke_re
                    "nke_lobster", "nke_olive", "nke_lime", "nke_sky", "nke_fuschia", 
                    "nke_violet", "nke_tan", "nke_berry", "nke_mint", "nke_sun"]
 
-var names = {2: ['Arousal', 'Manipulation'], 
-3: ['Arousal', 'Manipulation', 'Language'], 
-4: ['Memory', 'Arousal', 'Vision', 'Hearing'], 
-5: ['Memory', 'Arousal', 'Manipulation', 'Vision', 'Hearing'], 
-6: ['Memory', 'Reward', 'Reaction Time', 'Manipulation', 'Vision', 'Hearing'], 
-7: ['Emotion', 'Reward', 'Cognitive', 'Manipulation', 'Vision', 'Hearing', 'Meaning'], 
-8: ['Memory', 'Episodic Memory', 'Arousal', 'Cognitive Process', 'Manipulation', 'Vision', 'Perception', 'Language'], 
-9: ['Memory', 'Reward', 'Anticipation', 'Cognitive Process', 'Manipulation', 'Vision', 'Word', 'Perception', 'Language'], 
-10: ['Memory', 'Reward', 'Arousal', 'Cognitive', 'Manipulation', 'Episodic Memory', 'Recall', 'Vision', 'Perception', 'Language'], 
-11: ['Memory', 'Episodic Memory', 'Emotion', 'Cognitive', 'Anticipation', 'Arousal', 'Manipulation', 'Vision', 'Hearing', 'Language', 'Cognitive Process'], 
-12: ['Memory', 'Episodic Memory', 'Valence', 'Reward', 'Anticipation', 'Emotion', 'Arousal', 'Cognition', 'Manipulation', 'Vision', 'Hearing', 'Language'], 
-13: ['Memory', 'Episodic Memory', 'Emotion', 'Decision Making', 'Reward', 'Arousal', 'Cognitive', 'Manipulation', 'Recall', 'Vision', 'Perception', 'Hearing', 'Language'], 
-14: ['Memory', 'Retrieval', 'Episodic Memory', 'Decision Making', 'Reward', 'Arousal', 'Salience', 'Cognitive', 'Manipulation', 'Hand', 'Vision', 'Perception', 'Language', 'Emotion'], 
-15: ['Memory', 'Episodic Memory', 'Recall', 'Retrieval', 'Valence', 'Reward', 'Anticipation', 'Emotion', 'Cognition', 'Arousal', 'Manipulation', 'Cognitive', 'Vision', 'Hearing', 'Language'], 
-16: ['Memory', 'Episodic Memory', 'Emotion', 'Retrieval', 'Reward', 'Anticipation', 'Arousal', 'Thought', 'Cognitive', 'Preparation', 'Vision', 'Recognition', 'Perception', 'Language', 'Word', 'Meaning'], 
-17: ['Memory', 'Episodic Memory', 'Retrieval', 'Recall', 'Recognition', 'Arousal', 'Anticipation', 'Reward', 'Emotion', 'Cognitive', 'Preparation', 'Vision', 'Familiarity', 'Hearing', 'Language', 'Meaning', 'Representation'], 
-18: ['Memory', 'Episodic Memory', 'Recall', 'Retrieval', 'Word', 'Emotion', 'Decision Making', 'Cognitive', 'Reward', 'Arousal', 'Cognitive Process', 'Manipulation', 'Execution', 'Vision', 'Hearing', 'Language', 'Speech', 'Meaning'], 
-19: ['Memory', 'Episodic Memory', 'Recall', 'Recognition', 'Fear', 'Reward', 'Reaction Time', 'Anticipation', 'Retrieval', 'Emotion', 'Arousal', 'Cognitive', 'Manipulation', 'Vision', 'Hearing', 'Language Processing', 'Speech', 'Language', 'Word'], 
-20: ['Memory', 'Episodic Memory', 'Familiarity', 'Retrieval', 'Recall', 'Encoding', 'Emotion', 'Reward', 'Anticipation', 'Arousal', 'Cognitive Process', 'Cognitive', 'Execution', 'Manipulation', 'Preparation', 'Thought', 'Vision', 'Hearing', 'Language', 'Meaning'], 
-21: ['Memory', 'Episodic Memory', 'Recall', 'Retrieval', 'Emotion', 'Reward', 'Decision Making', 'Anticipation', 'Salience', 'Arousal', 'Attention', 'Context', 'Cognitive', 'Manipulation', 'Execution', 'Motor Control', 'Vision', 'Hearing', 'Word', 'Language', 'Meaning'], 
-22: ['Memory', 'Episodic Memory', 'Recall', 'Remembering', 'Recognition', 'Retrieval', 'Feedback', 'Fear', 'Emotion', 'Reward', 'Decision Making', 'Anticipation', 'Arousal', 'Cognitive', 'Manipulation', 'Cognitive Process', 'Vision', 'Perception', 'Language', 'Hearing', 'Word', 'Meaning'], 
-23: ['Memory', 'Episodic Memory', 'Recall', 'Recognition', 'Familiarity', 'Encoding', 'Remembering', 'Retrieval', 'Fear', 'Reward', 'Decision Making', 'Anticipation', 'Arousal', 'Emotion', 'Cognitive', 'Preparation', 'Vision', 'Perception', 'Hearing', 'Language', 'Word', 'Meaning', 'Manipulation'], 
-24: ['Memory', 'Episodic Memory', 'Retrieval', 'Recall', 'Encoding', 'Recognition', 'Feedback', 'Pain', 'Emotion', 'Salience', 'Reward', 'Decision Making', 'Valence', 'Anticipation', 'Arousal', 'Reaction Time', 'Execution', 'Cognitive Process', 'Perception', 'Vision', 'Speech', 'Language', 'Manipulation', 'Word'], 
-25: ['Memory', 'Episodic Memory', 'Encoding', 'Retrieval', 'Familiarity', 'Recognition', 'Recall', 'Feedback', 'Fear', 'Emotion', 'Reward', 'Decision Making', 'Anticipation', 'Arousal', 'Cognitive', 'Manipulation', 'Vision', 'Preparation', 'Social Cognition', 'Perception', 'Language', 'Hearing', 'Language Processing', 'Meaning', 'Word']}
+var names = { 2: ['Arousal', 'Manipulation'], 
+              3: ['Arousal', 'Manipulation', 'Language'], 
+              4: ['Memory', 'Arousal', 'Vision', 'Hearing'], 
+              5: ['Memory', 'Arousal', 'Manipulation', 'Vision', 'Hearing'], 
+              6: ['Memory', 'Reward', 'Reaction Time', 'Manipulation', 'Vision', 'Hearing'], 
+              7: ['Emotion', 'Reward', 'Cognitive', 'Manipulation', 'Vision', 'Hearing', 'Meaning'], 
+              8: ['Memory', 'Episodic Memory', 'Arousal', 'Cognitive Process', 'Manipulation', 'Vision', 'Perception', 'Language'], 
+              9: ['Memory', 'Reward', 'Anticipation', 'Cognitive Process', 'Manipulation', 'Vision', 'Word', 'Perception', 'Language'], 
+              10: ['Memory', 'Reward', 'Arousal', 'Cognitive', 'Manipulation', 'Episodic Memory', 'Recall', 'Vision', 'Perception', 'Language'], 
+              11: ['Memory', 'Episodic Memory', 'Emotion', 'Cognitive', 'Anticipation', 'Arousal', 'Manipulation', 'Vision', 'Hearing', 'Language', 'Cognitive Process'], 
+              12: ['Memory', 'Episodic Memory', 'Valence', 'Reward', 'Anticipation', 'Emotion', 'Arousal', 'Cognition', 'Manipulation', 'Vision', 'Hearing', 'Language'], 
+              13: ['Memory', 'Episodic Memory', 'Emotion', 'Decision Making', 'Reward', 'Arousal', 'Cognitive', 'Manipulation', 'Recall', 'Vision', 'Perception', 'Hearing', 'Language'], 
+              14: ['Memory', 'Retrieval', 'Episodic Memory', 'Decision Making', 'Reward', 'Arousal', 'Salience', 'Cognitive', 'Manipulation', 'Hand', 'Vision', 'Perception', 'Language', 'Emotion'], 
+              15: ['Memory', 'Episodic Memory', 'Recall', 'Retrieval', 'Valence', 'Reward', 'Anticipation', 'Emotion', 'Cognition', 'Arousal', 'Manipulation', 'Cognitive', 'Vision', 'Hearing', 'Language'], 
+              16: ['Memory', 'Episodic Memory', 'Emotion', 'Retrieval', 'Reward', 'Anticipation', 'Arousal', 'Thought', 'Cognitive', 'Preparation', 'Vision', 'Recognition', 'Perception', 'Language', 'Word', 'Meaning'], 
+              17: ['Memory', 'Episodic Memory', 'Retrieval', 'Recall', 'Recognition', 'Arousal', 'Anticipation', 'Reward', 'Emotion', 'Cognitive', 'Preparation', 'Vision', 'Familiarity', 'Hearing', 'Language', 'Meaning', 'Representation'], 
+              18: ['Memory', 'Episodic Memory', 'Recall', 'Retrieval', 'Word', 'Emotion', 'Decision Making', 'Cognitive', 'Reward', 'Arousal', 'Cognitive Process', 'Manipulation', 'Execution', 'Vision', 'Hearing', 'Language', 'Speech', 'Meaning'], 
+              19: ['Memory', 'Episodic Memory', 'Recall', 'Recognition', 'Fear', 'Reward', 'Reaction Time', 'Anticipation', 'Retrieval', 'Emotion', 'Arousal', 'Cognitive', 'Manipulation', 'Vision', 'Hearing', 'Language Processing', 'Speech', 'Language', 'Word'], 
+              20: ['Memory', 'Episodic Memory', 'Familiarity', 'Retrieval', 'Recall', 'Encoding', 'Emotion', 'Reward', 'Anticipation', 'Arousal', 'Cognitive Process', 'Cognitive', 'Execution', 'Manipulation', 'Preparation', 'Thought', 'Vision', 'Hearing', 'Language', 'Meaning'], 
+              21: ['Memory', 'Episodic Memory', 'Recall', 'Retrieval', 'Emotion', 'Reward', 'Decision Making', 'Anticipation', 'Salience', 'Arousal', 'Attention', 'Context', 'Cognitive', 'Manipulation', 'Execution', 'Motor Control', 'Vision', 'Hearing', 'Word', 'Language', 'Meaning'], 
+              22: ['Memory', 'Episodic Memory', 'Recall', 'Remembering', 'Recognition', 'Retrieval', 'Feedback', 'Fear', 'Emotion', 'Reward', 'Decision Making', 'Anticipation', 'Arousal', 'Cognitive', 'Manipulation', 'Cognitive Process', 'Vision', 'Perception', 'Language', 'Hearing', 'Word', 'Meaning'], 
+              23: ['Memory', 'Episodic Memory', 'Recall', 'Recognition', 'Familiarity', 'Encoding', 'Remembering', 'Retrieval', 'Fear', 'Reward', 'Decision Making', 'Anticipation', 'Arousal', 'Emotion', 'Cognitive', 'Preparation', 'Vision', 'Perception', 'Hearing', 'Language', 'Word', 'Meaning', 'Manipulation'], 
+              24: ['Memory', 'Episodic Memory', 'Retrieval', 'Recall', 'Encoding', 'Recognition', 'Feedback', 'Pain', 'Emotion', 'Salience', 'Reward', 'Decision Making', 'Valence', 'Anticipation', 'Arousal', 'Reaction Time', 'Execution', 'Cognitive Process', 'Perception', 'Vision', 'Speech', 'Language', 'Manipulation', 'Word'], 
+              25: ['Memory', 'Episodic Memory', 'Encoding', 'Retrieval', 'Familiarity', 'Recognition', 'Recall', 'Feedback', 'Fear', 'Emotion', 'Reward', 'Decision Making', 'Anticipation', 'Arousal', 'Cognitive', 'Manipulation', 'Vision', 'Preparation', 'Social Cognition', 'Perception', 'Language', 'Hearing', 'Language Processing', 'Meaning', 'Word']}
 
 function pad(number) { return (number < 10 ? '0' : '') + number };
 
@@ -64,14 +64,13 @@ function loadBrainMap(k) {
                         'url': 'data/k' + pad(k) + '/circuit_k' + pad(k) + '_dom' + pad(i) + '.nii.gz',
                         'name': names[k][i-1],
                         'colorPalette': nke_palette[i-1],
-                        'opacity': 0.6,
+                        'opacity': 0.5,
                         'cache': false,
                         'val': 0.5
                     })
         };
     viewer.loadImages(images);
 
-    //document.getElementById('selected').background = "red";
 };
 
 jQuery(document).ready(function() {
